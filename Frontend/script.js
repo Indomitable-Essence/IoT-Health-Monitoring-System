eaa19b183b91e47674950d2c067dc183befb439d
Imdomitable-Essence <azeezatakingboye43@gmail.com>
2026-09-09 11:14:08 +0100

    Use secure MQTT WebSocket for dashboard

--- Frontend/script.js
+++ Frontend/script.js
@@ -152,7 +152,7 @@ console.log("JavaScript connected!");
 //         }
 //     }
 // });
-const client = mqtt.connect('ws://broker.hivemq.com:8000/mqtt');
+const client = mqtt.connect('wss://broker.hivemq.com:8884/mqtt');
 
 client.on('connect', () => {
     console.log("MQTT Connected");
