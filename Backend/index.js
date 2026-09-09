6e73b56d8fb4ab5a3e025ed2d1d36a1b942e65d6
Imdomitable-Essence <azeezatakingboye43@gmail.com>
2026-09-09 11:23:21 +0100

    Allow Vercel frontend in CORS

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,8 @@ const app = express();
 app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://iot-health-monitoring-system-three.vercel.app"
     ],
     credentials: true
 }));
