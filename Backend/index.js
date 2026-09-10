6ac89dea8dc0b66f0b499d9a7b31a649fec0c028
Imdomitable-Essence <azeezatakingboye43@gmail.com>
2026-09-10 15:30:18 +0100

    Fix CORS for Vercel frontend

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://elaz-hms.vercel.app/"
+        "https://elaz-hms.vercel.app"
     ],
     credentials: true
 }));
